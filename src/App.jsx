e16e2895aa5779f46e03d0d8a5add81386b97838
Umasanjuk <sanjukambhampati5200231@gmail.com>
2025-05-29 11:25:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Fackproduct from "./FackProducts/FackProduct.jsx";
+const App = () => {
+  return (
+    <div>
+    <Fackproduct />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/FackProducts/FackProduct.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import data from './Fackdata.jsx'
+import './product.css'
+
+function Fackproduct() {
+  const recipes = data[0].recipes;
+
+  return (
+    <div>
+      <h2 style={{textAlign: "center"}}>Recipe List</h2>
+      <div className="recipe-list">
+        {recipes.map(recipe => (
+          <div className="recipe-card" key={recipe.id}>
+            <img src={recipe.image} alt={recipe.name} />
+            <h3>{recipe.name}</h3>
+            <p><strong>Cuisine:</strong> {recipe.cuisine}</p>
+            {/* <p><strong>Difficulty:</strong> {recipe.difficulty}</p> */}
+            {/* <p><strong>Rating:</strong> {recipe.rating} ⭐</p> */}
+            {/* <p><strong>Tags:</strong> {recipe.tags.join(', ')}</p> */}
+            <p><strong>mealType:</strong> {recipe.mealType.join(', ')}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Fackproduct;
+
+
